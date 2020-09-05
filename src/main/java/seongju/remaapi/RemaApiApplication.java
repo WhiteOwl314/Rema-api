3d7e941345676a45adf76e673fbb3392faaea750
ParkSeongju <parksj914@naver.com>
2020-09-05 23:02:05 +0900

    start

---
+++ src/main/java/seongju/remaapi/RemaApiApplication.java
@@ -0,0 +1,13 @@
+package seongju.remaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemaApiApplication.class, args);
+    }
+
+}
