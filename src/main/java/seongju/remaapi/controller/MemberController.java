7373ca97900e34a387cf00feb9d6737fefa6a9c1
ParkSeongju <parksj914@naver.com>
2020-09-07 01:03:26 +0900

    member-checkId

--- src/main/java/seongju/remaapi/controller/MemberController.java
+++ src/main/java/seongju/remaapi/controller/MemberController.java
@@ -2,11 +2,10 @@ package seongju.remaapi.controller;
 
 import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import seongju.remaapi.service.MemberService;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping(value = "/member")
@@ -14,13 +13,15 @@ public class MemberController {
     @Autowired
     private MemberService memberService;
 
+    @CrossOrigin("*")
     @RequestMapping(
             value = "/checkId.do",
             method = RequestMethod.POST
     )
     public String checkId(
-            @RequestParam("id") String id
+            @RequestBody HashMap<String, Object> map
     ){
+        String id = (String) map.get("id");
         return memberService.checkId(id);
     }
 }
